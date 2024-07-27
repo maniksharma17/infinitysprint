b39a81f8a850f062aaddcba5eae37f1327bfd656
Manik Sharma <maniksharma.rke@gmail.com>
2024-07-28 03:32:42 +0530

    error resolved

--- components/Form.tsx
+++ components/Form.tsx
@@ -11,8 +11,8 @@ const Form = () => {
 
   return <div className='bg-white border border-purple-800 shadow-2xl rounded-xl z-50 flex justify-center w-fit gap-6 fixed top-0 m-auto max-md:translate-y-1/4 max-md:translate-x-7 translate-x-[62%] translate-y-20'> 
       <Button className='absolute top-2 bg-purple-800 right-2' onClick={()=>{setFormOpen(!formOpen)}}>Close</Button>
-      <iframe className='max-md:hidden' src="https://docs.google.com/forms/d/e/1FAIpQLSenr-YhcNRdihAzyB30U2IQcw6oYTPi9qX2cv5twMJ3RJhYhw/viewform?embedded=true" width="640" height="640" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-      <iframe className='md:hidden' src="https://docs.google.com/forms/d/e/1FAIpQLSenr-YhcNRdihAzyB30U2IQcw6oYTPi9qX2cv5twMJ3RJhYhw/viewform?embedded=true" width="350" height="450" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>    </div>
+      <iframe className='max-md:hidden' src="https://docs.google.com/forms/d/e/1FAIpQLSenr-YhcNRdihAzyB30U2IQcw6oYTPi9qX2cv5twMJ3RJhYhw/viewform?embedded=true" width="640" height="640">Loading…</iframe>
+      <iframe className='md:hidden' src="https://docs.google.com/forms/d/e/1FAIpQLSenr-YhcNRdihAzyB30U2IQcw6oYTPi9qX2cv5twMJ3RJhYhw/viewform?embedded=true" width="350" height="450">Loading…</iframe>    </div>
 }
 
 const FormItem = ({label}: {label: string}) => {
