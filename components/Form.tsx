655d0bd0ca358eb2e2c525e75779995f6daa58bb
Manik Sharma <maniksharma.rke@gmail.com>
2024-07-28 03:39:17 +0530

    error resolved

--- components/Form.tsx
+++ components/Form.tsx
@@ -9,7 +9,7 @@ const Form = () => {
   const [formOpen, setFormOpen] = useRecoilState(formAtom);
   if(!formOpen) return <></>
 
-  return <div className='bg-white border border-purple-800 shadow-2xl rounded-xl z-50 flex justify-center w-fit gap-6 fixed top-0 m-auto max-md:translate-y-1/4 max-md:translate-x-7 translate-x-[62%] translate-y-20'> 
+  return <div className='bg-white border border-purple-800 shadow-2xl rounded-xl z-50 flex justify-center w-fit gap-6 fixed top-0 m-auto max-md:translate-y-1/4 max-md:translate-x-0 max-md:w-full translate-x-[63%] translate-y-20'> 
       <Button className='absolute top-2 bg-purple-800 right-2' onClick={()=>{setFormOpen(!formOpen)}}>Close</Button>
       <iframe className='max-md:hidden' src="https://docs.google.com/forms/d/e/1FAIpQLSenr-YhcNRdihAzyB30U2IQcw6oYTPi9qX2cv5twMJ3RJhYhw/viewform?embedded=true" width="640" height="640">Loading…</iframe>
       <iframe className='md:hidden' src="https://docs.google.com/forms/d/e/1FAIpQLSenr-YhcNRdihAzyB30U2IQcw6oYTPi9qX2cv5twMJ3RJhYhw/viewform?embedded=true" width="350" height="450">Loading…</iframe>    </div>
